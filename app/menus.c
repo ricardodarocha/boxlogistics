2c7493e8ed44662958eacb05f0d1d60b45105a5e
Ricardo da Rocha <ricardodarocha@outlook.com>
2025-11-03 18:58:31 -0300

    Add files via upload

--- app/menus.c
+++ app/menus.c
@@ -19,52 +19,50 @@ void boas_vindas_simplificado(const char *title) {
 }
 
 const char *menu_principal =
-    "(1) Cadastrar produtos\n"
-    "(2) Cadastrar entregas\n"
-    "(3) Cadastrar cargas\n"
-    "(4) Formulario de vendas\n"
-    "(5) Relatorios\n"
-    "________________________\n"
-    "(9) Configuracoes\n"
-    "(0) Sair\n";
+    "   (1) Cadastrar produtos     (4) Formulario de vendas         \n"
+    "   (2) Cadastrar entregas     (5) Relatorios                   \n"
+    "   (3) Cadastrar cargas                                        \n"
+    " ═══════════════════════════════════════════════════════════════════\n"
+    "   (9) Configuracoes          (0) Sair\n"
+    "";
 
 const char *menu_cadastrar_produtos =
-    "(1) Inserir produto\n"
-    "(2) Buscar produto\n"
-    "(3) Listar produtos\n"
-    "________________________\n"
-    "(9) Configuracoes\n"
-    "(0) Voltar\n";
+    "   (1) Inserir produto\n"
+    "   (2) Buscar produto\n"
+    "   (3) Listar produtos\n"
+    " ═══════════════════════════════════════\n"
+    "   (9) Configuracoes\n"
+    "   (0) Voltar\n";
 
 const char *menu_cadastrar_entregas =
-    "(1) Inserir entrega\n"
-    "(2) Buscar entrega\n"
-    "(3) Listar entregas\n"
-    "________________________\n"
-    "(9) Configuracoes\n"
-    "(0) Voltar\n";
+    "   (1) Inserir entrega\n"
+    "   (2) Buscar entrega\n"
+    "   (3) Listar entregas\n"
+    " ═══════════════════════════════════════\n"
+    "   (9) Configuracoes\n"
+    "   (0) Voltar\n";
 
 const char *menu_cadastrar_cargas =
-    "(1) Inserir carga\n"
-    "(2) Buscar carga\n"
-    "(3) Listar cargas\n"
-    "________________________\n"
-    "(9) Configuracoes\n"
-    "(0) Voltar\n";
+    "   (1) Inserir carga\n"
+    "   (2) Buscar carga\n"
+    "   (3) Listar cargas\n"
+    " ═══════════════════════════════════════\n"
+    "   (9) Configuracoes\n"
+    "   (0) Voltar\n";
 
 const char *menu_vendas =
-    "(1) Inserir venda\n"
-    "(2) Buscar venda\n"
-    "(3) Listar vendas\n"
-    "________________________\n"
-    "(9) Configuracoes\n"
-    "(0) Voltar\n";
+    "   (1) Inserir venda\n"
+    "   (2) Buscar venda\n"
+    "   (3) Listar vendas\n"
+    " ═══════════════════════════════════════\n"
+    "   (9) Configuracoes\n"
+    "   (0) Voltar\n";
 
 const char *menu_relatorio =
-    "(1) Vendas do Dia\n"
-    "(2) Estoque Atual\n"
-    "________________________\n"
-    "(0) Voltar\n";
+    "   (1) Vendas do Dia\n"
+    "   (2) Estoque Atual\n"
+    " ═══════════════════════════════════════\n"
+    "   (0) Voltar\n";
 
 void cadastrar_produtos() {
     int operacao =  exibir_menu(menu_cadastrar_produtos);
